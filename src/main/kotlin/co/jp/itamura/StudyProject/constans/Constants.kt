f5683e26addb8dce4877ea3019f47733d821c008
haryu-ta <dummy.git@github.com>
2020-01-20 23:22:28 +0900

    ファイル戻し

---
+++ src/main/kotlin/co/jp/itamura/StudyProject/constans/Constants.kt
@@ -0,0 +1,7 @@
+package co.jp.itamura.StudyProject.constans
+
+val P_FILEPATH : String = "/Users/xxxxxxxxxxxxx/Documents/workspace_php/api/prefucture/"
+val L_FILEPATH : String = "/Users/xxxxxxxxxxxxx/Documents/workspace_php/api/line/"
+val S_FILEPATH : String = "/Users/xxxxxxxxxxxxx/Documents/workspace_php/api/station/"
+val G_FILEPATH : String = "/Users/xxxxxxxxxxxxx/Documents/workspace_php/api/group/"
+val J_FILEPATH : String = "/Users/xxxxxxxxxxxxx/Documents/workspace_php/api//join/"
